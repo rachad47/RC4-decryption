e005e475c1abb2ab99260dac189ba0eef57b64e2
rachad47 <mourachad@gmail.com>
2024-06-12 16:52:34 -0700

    fff

--- rtl/mem_init.v
+++ rtl/mem_init.v
@@ -2,11 +2,11 @@ module meme_init (
     input logic clk, 
     input logic state_start,
     //
-    output logic finish, meme_init_start,
+    output logic finish, init_mem_handler,
     output logic [7:0] data, 
-    output logic [7:0] address,
-    output logic [1:0] sel,
-    output logic wen
+    output logic [7:0] address,           
+    output logic [1:0] memory_sel,           // memory select (ROM, RAM1, RAM2)
+    output logic wen                         // write enable
 );
 
 logic [8:0] i; // i as internal signal
@@ -14,14 +14,14 @@ logic [8:0] i; // i as internal signal
 always_ff @(posedge clk) begin
     if (state_start) begin
         i <= 8'h00;
-        meme_init_start <= 1;
+        init_mem_handler <= 1;
 
     end else begin
         if (i == 9'h100) begin  // 9'h100 = 256 , the idea here is to stop at 256 in way we can read 255
             finish <= 1;
             wen <= 0;
-            meme_init_start <= 0;
-            
+            init_mem_handler <= 0;
+
         end else begin
             finish <= 0;
             i <= i + 1;
@@ -33,6 +33,6 @@ end
 
 assign data = i;
 assign address = i;
-assign sel = 2'b01;
+assign memory_sel = 2'b01;
 
 endmodule
