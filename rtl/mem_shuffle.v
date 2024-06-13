83ceb220fc560a1c33395e53a44a7cdb423131aa
rachad47 <mourachad@gmail.com>
2024-06-13 14:08:18 -0700

    d

--- rtl/mem_shuffle.v
+++ rtl/mem_shuffle.v
@@ -1,9 +1,8 @@
 module mem_shuffle (
     input logic clk, 
-    input logic state_start
+    input logic state_start,
     input logic [23:0] secret_key,
     input logic [7:0] q_data,
-
     output logic finish, shuffle_mem_handler,
     output logic [7:0] data,
     output logic [7:0] address,
@@ -12,13 +11,13 @@ module mem_shuffle (
 );
     logic [8:0] i,j; // i as internal signal
     
-    localparam START = 3b'000;
-    localparam READ_i = 3b'001;
-    localparam ADD = 3b'010;
-    localparam READ_j = 3b'011;
-    localparam CHANGE_i = 3b'100;       //CHANGE_i & CHANGE_j for SWAP
-    localparam CHANGE_j = 3b'101;
-    localparam FINISH = 3b'110;
+    localparam START = 3'b000;
+    localparam READ_i = 3'b001;
+    localparam ADD = 3'b010;
+    localparam READ_j = 3'b011;
+    localparam CHANGE_i = 3'b100;       //CHANGE_i & CHANGE_j for SWAP
+    localparam CHANGE_j = 3'b101;
+    localparam FINISH = 3'b110;
 
     logic [2:0] state;
     logic [7:0] temp_i, temp_j;
@@ -35,28 +34,33 @@ module mem_shuffle (
                     address <= i;
                     memory_sel <= 2'b01;
                     wen <= 0;
+                    shuffle_mem_handler <= 1;
                 end
                 READ_i: begin
                     state <= ADD;
                     address <= i;
                     temp_i <= q_data;
                     wen <= 0;
+                    shuffle_mem_handler <= 1;
                 end
                 ADD: begin
                     state <= READ_j;
                     j <= j + temp_i + secret_key[i%24];
                     wen <= 0; 
+                    shuffle_mem_handler <= 1;
                 end
                 READ_j: begin
                     state <= CHANGE_i;
                     address <= j;
                     temp_j <= q_data;
+                    shuffle_mem_handler <= 1;
                 end
                 CHANGE_i: begin
                     state <= CHANGE_j;
                     address <= i;
                     data <= temp_j;
                     wen <= 1;
+                    shuffle_mem_handler <= 1;
 
                 end
                 CHANGE_j: begin
@@ -64,6 +68,7 @@ module mem_shuffle (
                     address <= j;
                     data <= temp_i;
                     wen <= 1;
+                    shuffle_mem_handler <= 1;
 
                 end
                 FINISH: begin
@@ -72,9 +77,11 @@ module mem_shuffle (
                         shuffle_mem_handler <= 0;
                         memory_sel <= 2'b00;
                         wen <= 0; 
+                        shuffle_mem_handler <= 0;
                     end else begin
                         state <= START;
                         i <= i + 1;
+                        finish <= 0;
                     end
                     
                     
@@ -83,10 +90,6 @@ module mem_shuffle (
         end
     end
 
-
-
-
-    
 
 
 endmodule
