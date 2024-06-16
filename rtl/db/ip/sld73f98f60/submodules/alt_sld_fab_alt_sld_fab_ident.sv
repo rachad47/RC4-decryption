c7e49c0891326fbbb36cd72eea87a06de1f7d0d6
paulo <paulolin2007@gmail.com>
2024-06-16 14:55:51 -0700

    signal tap

--- rtl/db/ip/sld73f98f60/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ rtl/db/ip/sld73f98f60/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : b9e72c39d444d112acc3
+//   DESIGN_HASH   : 10a161397d3cabc87ced
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb9e72c39d444d112acc3 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h10a161397d3cabc87ced };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
